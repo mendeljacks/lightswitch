00edf25bab41519cd2d03fc465e32839827ac4dd
Mendel <mendeljacks@gmail.com>
2020-03-06 15:00:23 -0500

    url

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 export const API_URL ='https://redibase-lightserver.herokuapp.com/'
+// export const API_URL ='http://localhost:4411'
 
 var apiAxios = axios.create({ baseURL: API_URL })
 
